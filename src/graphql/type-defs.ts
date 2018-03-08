588068e5965b0fce8f8e8732ac85501d8b1fb4e3
Rafael Ribeiro <rafael.correia.poli@gmail.com>
2018-03-07 22:39:58 -0300

    Schema type-defs

--- src/graphql/type-defs.ts
+++ src/graphql/type-defs.ts
@@ -1,8 +1,80 @@
 export const typeDefs = `
+enum OrderType {
+  TAKE_TO_CLIENT
+  TAKE_FROM_CLIENT
+}
+
+enum OrderStatus {
+  ORDERED
+  ALLOCATED
+  CANCELLED
+  DELIVERED
+}
+
+enum DeliveryStatus {
+  AUCTIONED
+  ACCEPTED
+  FINISHED
+  CANCELLED
+}
+
+type Customer {
+  id: ID!
+  name: String!
+}
+
+type Location {
+  lat: Float!
+  lng: Float!
+}
+
+
+type Carrier {
+  id: ID!
+  name: String!
+  imageUrl: String!
+}
+
+type Order {
+  id: ID!
+  customer: Customer!
+  sourceLocation: Location!
+  destLocation: Location!
+  type: OrderType!
+  deliveryInstructions: String
+  withdrawalInstructions: String
+  contactNumber: String!
+  status: OrderStatus!
+  magicWord: String!
+  carrier: Carrier
+}
+type Delivery {
+  carrier: Carrier!
+  status: DeliveryStatus
+  orders: [Order!]!
+}
+
+type CreateOrderPayload {
+  order: Order!
+}
+
 type Query {
   hello: String!
+  getAuctionedDeliveries: String!
 }
+
+input CreateOrderInput {
+  sourceLocation: Location!
+  destLocation: Location!
+  type: OrderType!
+  deliveryInstructions: String
+  withdrawalInstructions: String
+  contactNumber: String!
+}
+
 type Mutation {
-  bye: String!
+  confirmDeliveredOrder(magicWord: String!): Boolean
+  createOrder(input: CreateOrderInput!): CreateOrderPayload
+  order(id: ID!): Order
 }
 `
