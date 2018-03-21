0a6642dfa1f45221eb71caaf429acac4d6093f0b
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2018-03-21 15:45:51 -0300

    Implement getPricing query

--- src/pricing/resolvers/query.ts
+++ src/pricing/resolvers/query.ts
@@ -1,4 +1,6 @@
 import { IContext } from '../../routes';
+import * as distance from 'google-distance';
+import * as util from 'util';
 
 // {
 //   getPricing(
@@ -6,9 +8,29 @@ import { IContext } from '../../routes';
 //     destLocation:{lat:1, lng:2}
 //   )
 // }
+
+distance.apiKey = process.env.GOOGLE_API_KEY
+const formatLocation = (location) => util.format('%f,%f', location.lat, location.lng)
+const pricingFromDistance = (distance) => distance.durationValue / 60 * 0.25 + distance.distanceValue / 1000 * 1.4
+
+const getDistance = (params) => new Promise((resolve, reject) => {
+  distance.get(
+    params,
+    function(err, data) {
+      if (err)
+        reject(err)
+      else
+        resolve(data)
+  });
+})
+
 export const Query = {
-  // TODO
   getPricing: async (value, { sourceLocation, destLocation }, { components }: IContext)  => {
-    return 15.23
+    return getDistance(
+      {
+        origin: formatLocation(sourceLocation),
+        destination: formatLocation(destLocation),
+      })
+      .then(pricingFromDistance)
   },
 }
